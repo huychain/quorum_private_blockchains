5594b191dacfa574bd3ea31c96808b1a93419abe
Dang Quoc Huy <huysamdua@yahoo.com>
2017-10-14 07:37:26 +0700

    configure Truffle to point to our running Quorum client

--- truffle.js
+++ truffle.js
@@ -1,8 +1,9 @@
+// File: 'truffle.js' (edited for 7nodes example)
 module.exports = {
   networks: {
     development: {
       host: "localhost",
-      port: 8545,
+      port: 22000, // was 8548
       network_id: "*" // Match any network id
     }
   }
