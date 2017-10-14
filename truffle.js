4202f1b15a85c9b09d806509e2e03959369b00cb
Dang Quoc Huy <huysamdua@yahoo.com>
2017-10-14 10:52:18 +0700

    add other network configurations

--- truffle.js
+++ truffle.js
@@ -1,9 +1,19 @@
-// File: 'truffle.js' (edited for 7nodes example)
+// File: 'truffle.js'
 module.exports = {
   networks: {
     development: {
       host: "localhost",
       port: 22000, // was 8548
+      network_id: "*" // Match any network id
+    },
+    nodefour: {
+      host: "localhost",
+      port: 22003,
+      network_id: "*" // Match any network id
+    },
+    nodeseven: {
+      host: "localhost",
+      port: 22006,
       network_id: "*" // Match any network id
     }
   }
