fc448ec6e49fe2032a4cce2a0eeb74226672afc3
Dang Quoc Huy <huysamdua@yahoo.com>
2017-10-14 07:43:41 +0700

    create simple contract

---
+++ contracts/SimpleStorage.sol
@@ -0,0 +1,18 @@
+// File: `./contracts/SimpleStorage.sol`
+pragma solidity ^0.4.8;
+
+contract SimpleStorage {
+  uint public storedData;
+
+  function SimpleStorage(uint initVal) {
+    storedData = initVal;
+  }
+
+  function set(uint x) {
+    storedData = x;
+  }
+
+  function get() constant returns (uint retVal) {
+    return storedData;
+  }
+}
